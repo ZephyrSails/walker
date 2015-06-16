cfb48a4cf5fa423fb92f61d6c6686cec2d62ff73
Zephyr Sails <zephyrsails@gmail.com>
2015-06-16 17:35:10 +0800

    百度地图定位
    现在户外模式已经可以定位了

--- src/com/example/walker/OutdoorActivity.java
+++ src/com/example/walker/OutdoorActivity.java
@@ -1,11 +1,25 @@
 package com.example.walker;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+//import com.baidu.location.LocationClientOption.LocationMode;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,8 +31,19 @@ public class OutdoorActivity extends ActionBarActivity implements
 	// Used to store the last screen title. For use in
 	// {@link #restoreActionBar()}.
 	private CharSequence mTitle;
-
+	// 百度地图相关
+	private LocationClient mLocationClient = null;
+	private BDLocationListener myListener = new MyLocationListener();
+	private LocationMode mCurrentMode = LocationMode.NORMAL;
+	private BitmapDescriptor mCurrentMarker;
+	
 	private MapView mMapView = null;
+	private BaiduMap mBaiduMap = null;
+	
+	private boolean isFirstLoc = true;
+	//private CurrentMaker mCurrentMarker
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +53,19 @@ public class OutdoorActivity extends ActionBarActivity implements
 		SDKInitializer.initialize(getApplicationContext());
 		setContentView(R.layout.activity_outdoor);
 		mMapView = (MapView) findViewById(R.id.bmapView);
+		mBaiduMap = mMapView.getMap();
+		
+	    // 开启定位图层  
+	    mBaiduMap.setMyLocationEnabled(true);
+	    // 定位初始化
+	    mLocationClient = new LocationClient(getApplicationContext());	//声明LocationClient类
+	    mLocationClient.registerLocationListener(myListener);	//注册监听函数
+	    LocationClientOption option = new LocationClientOption();
+	    option.setOpenGps(true);
+	    option.setCoorType("bd09ll");
+	    option.setScanSpan(1000);
+	    mLocationClient.setLocOption(option);
+	    mLocationClient.start();
 
 		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.navigation_drawer);
@@ -61,7 +99,50 @@ public class OutdoorActivity extends ActionBarActivity implements
 	public void onNavigationDrawerItemSelected(int position) {
 		// TODO Auto-generated method stub
 	}
-
+	
+	public class MyLocationListener implements BDLocationListener {
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			if (location == null)
+		            return ;
+			StringBuffer sb = new StringBuffer(256);
+			sb.append("time : ");
+			sb.append(location.getTime());
+			sb.append("\nerror code : ");
+			sb.append(location.getLocType());
+			sb.append("\nlatitude : ");
+			sb.append(location.getLatitude());
+			sb.append("\nlontitude : ");
+			sb.append(location.getLongitude());
+			sb.append("\nradius : ");
+			sb.append(location.getRadius());
+			if (location.getLocType() == BDLocation.TypeGpsLocation){
+				sb.append("\nspeed : ");
+				sb.append(location.getSpeed());
+				sb.append("\nsatellite : ");
+				sb.append(location.getSatelliteNumber());
+			} else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+				sb.append("\naddr : ");
+				sb.append(location.getAddrStr());
+			}
+			Log.i ("BDmap", sb.toString());
+			
+			MyLocationData locData = new MyLocationData.Builder()
+					.accuracy(location.getRadius())
+					.direction(100).latitude(location.getLatitude())
+					.longitude(location.getLongitude()).build();
+			mBaiduMap.setMyLocationData(locData);
+			if (isFirstLoc) {
+				isFirstLoc = false;
+				LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+				MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+				mBaiduMap.animateMapStatus(u);
+			}
+		}
+		public void onREceivePoi(BDLocation poiLocation) {
+			
+		}
+	}
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
